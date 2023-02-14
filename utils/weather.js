9eaadafab6f637d716375c6d519276c297a576be
Paul Cutmore <paul.cutmore@icloud.com>
2023-02-14 12:14:58 +0000

    Other(weather): Converted to use ';' terminated code.

--- utils/weather.js
+++ utils/weather.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 
 module.exports = async (location = "Gateshead") => {
     const res1 = await fetch("https://wttr.in/" + location + "?format=4");
-    const res2 = await res1.text()
-    console.log(res2)
-    return res2
+    const res2 = await res1.text();
+    console.log(res2);
+    return res2;
 }
